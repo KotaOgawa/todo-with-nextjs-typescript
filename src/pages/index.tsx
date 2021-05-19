7cb7408ff6ef4ae1c583fb8126f5a5fd02ebd261
KotaOgawa <k.ogawa.biz406@gmail.com>
2021-05-19 15:07:15 +0900

    :lipstick: Add or update the UI and style files.
    最低限のCSSを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,32 +13,32 @@ const Home: VFC = () => {
   return (
     <Layout>
       {/* タスク入力部 */}
-      <div className="">
+      <div className="flex h-12 w-80 p-2 m-2 bg-blue-100 rounded">
         <input type="text" className="py-0.5 border border-gray-500 rounded-sm shadow-sm cursor-pointer" />
-        <button type="button" className="border-gray-500 btn">
+        <button type="button" className="border-gray-500 bg-white btn">
           追加
         </button>
       </div>
 
       {/* 未完了のタスク */}
-      <div>
-        <p>未完了のTODO</p>
+      <div className="w-80 p-2 m-2 bg-yellow-100 rounded">
+        <p className="mb-2 text-lg font-medium text-center">未完了のTODO</p>
         <ul>
           <div className="flex items-center">
-            <li className="pl-5 list-disc list-inside">リストアイテム</li>
-            <button className="text-blue-600 border-blue-500 btn">完了</button>
-            <button className="text-red-600 border-red-500 btn">削除</button>
+            <li className="pl-5 list-decimal list-inside">リストアイテム</li>
+            <button className="btn border-gray-500">完了</button>
+            <button className="btn border-gray-500">削除</button>
           </div>
         </ul>
       </div>
 
       {/* 完了済みのタスク */}
-      <div>
-        <p>完了済みのTODO</p>
+      <div className="w-80 p-2 m-2 bg-pink-100 rounded">
+        <p className="mb-2 text-lg font-medium text-center">完了済みのTODO</p>
         <ul>
           <div className="flex items-center">
-            <li className="pl-5 list-decimal list-inside ">リストアイテム</li>
-            <button className="text-green-600 border-green-500 btn">戻す</button>
+            <li className="pl-5 list-disc list-inside ">リストアイテム</li>
+            <button className="btn border-gray-500">戻す</button>
           </div>
         </ul>
       </div>
